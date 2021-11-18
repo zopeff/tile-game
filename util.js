c012d564d5dd79d9a3a5b92937014d1ca25e9df7
Craig Schumann <cschumann@intralinks.com>
2021-11-18 08:15:51 -0500

    update

--- util.js
+++ util.js
@@ -25,3 +25,29 @@ Math.normalizeVector = function (v) {
     var m = Math.sqrt(v.x * v.x + v.y * v.y);
     Math.scaleVector(v, 1 / m);
 }
+
+// Returns true if point P inside the triangle with vertices at A, B and C
+// representing 2D vectors and points as [x,y]. Based on                        
+// http://www.blackpawn.com/texts/pointinpoly/default.html
+Math.pointInTriangle = function(P, A, B, C) {
+    // Compute vectors        
+    function vec(from, to) {  return [to[0] - from[0], to[1] - from[1]];  }
+    function dot(u, v) {  return u[0] * v[0] + u[1] * v[1];  }
+        
+    var v0 = vec(A, C);
+    var v1 = vec(A, B);
+    var v2 = vec(A, P);
+    // Compute dot products
+    
+    var dot00 = dot(v0, v0);
+    var dot01 = dot(v0, v1);
+    var dot02 = dot(v0, v2);
+    var dot11 = dot(v1, v1);
+    var dot12 = dot(v1, v2);
+    // Compute barycentric coordinates
+    var invDenom = 1.0 / (dot00 * dot11 - dot01 * dot01);
+    var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+    var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+    // Check if point is in triangle
+    return (u >= 0) && (v >= 0) && (u + v < 1);
+}
